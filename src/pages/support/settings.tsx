acbe5681157e8db077563f377c2712214339e82d
prabu-ssb <prabakar.venkataraman@ssb.no>
2020-10-15 22:54:22 +0200

    FBU-62 : i18n basic setup

--- src/pages/support/settings.tsx
+++ src/pages/support/settings.tsx
@@ -1,21 +1,52 @@
 import { useRouter } from 'next/router';
+import { RadioGroup } from '@statisticsnorway/ssb-component-library';
+import { i18n, withTranslation } from '../../../i18n';
 import SettingsPanel from '../../components/blocks/SettingsPanel';
 import { WorkspacePanel } from '../../components/layout/Workspace';
+import { LANGUAGES } from '../../utils/jsUtils';
 
-const Settings = () => {
+const Settings = ({t}) => {
     const router = useRouter();
 
     return (
-        <WorkspacePanel>
-            <h3>Innstillinger</h3>
-            <SettingsPanel
-                title="PIN"
-                description="Endre PIN"
-                onClick={() => { router.push('/support/manage-pin')}}
-            />
-        </WorkspacePanel>
+        <>
+            <h3>{t('title')}</h3>
+            <p>{t('test')}</p>
+            <WorkspacePanel>
+                <SettingsPanel
+                    title={t('pin.title')}
+                    description={t('pin.description')}
+                    onClick={() => {
+                        router.push('/support/manage-pin')
+                    }}
+                />
+                <SettingsPanel
+                    expandable={true}
+                    title={t('language.title')}
+                    description={LANGUAGES[i18n.language]}
+                >
+                    <RadioGroup
+                        onChange={(newLang) => {
+                            i18n.changeLanguage(newLang);
+                        }}
+                        selectedValue={i18n.language}
+                        orientation="column"
+                        items={Object.keys(LANGUAGES).map(key => ({
+                                label: LANGUAGES[key],
+                                value: key,
+                            })
+                        )}
+                    />
+                </SettingsPanel>
+            </WorkspacePanel>
+        </>
     );
-}
+};
 
-export default Settings;
+Settings.getInitialProps = async () => ({
+    namespacesRequired: ['settings'],
+});
+
+
+export default withTranslation('settings')(Settings);
 
