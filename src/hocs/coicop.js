0203b70e60230f2a03068efc9de017ef2973290a
Lægreid <evd@ssb.no>
2020-10-19 12:40:47 +0200

    change searchterm endpoint

--- src/hocs/coicop.js
+++ src/hocs/coicop.js
@@ -1,13 +1,14 @@
-import { useState } from 'react';
-import useAxios from 'axios-hooks';
+import { useState } from 'react'
+import useAxios from 'axios-hooks'
 
-const SURVEY_SERVICE_HOST = process.env.NEXT_PUBLIC_SURVEY_SERVICE_HOST || 'http://localhost:5555';
-const COICOP_SEARCH_ENDPOINT = `${SURVEY_SERVICE_HOST}/v1/items/search`;
+const SURVEY_SERVICE_HOST =
+  process.env.NEXT_PUBLIC_SURVEY_SERVICE_HOST || 'http://localhost:5555'
+const COICOP_SEARCH_ENDPOINT = `${SURVEY_SERVICE_HOST}/v1/searchterm`
 
 export const useCoicopSearch = (term) => {
-    const [coicopSearchTerm, setCoicopSearchTerm] = useState(term);
+  const [coicopSearchTerm, setCoicopSearchTerm] = useState(term)
 
-    /*
+  /*
     const COICOP_CONFIG = {
         method: 'GET',
         url: `${COICOP_SEARCH_ENDPOINT}?query=${coicopSearchTerm}`,
@@ -20,20 +21,22 @@ export const useCoicopSearch = (term) => {
     const { response: coicopFetchResponse, loading, error, refetch } = useAxios(COICOP_CONFIG);
     */
 
-    const searchUrl = `${COICOP_SEARCH_ENDPOINT}?query=${coicopSearchTerm}`;
-    const [{ data, loading, error}, refetch] = useAxios(searchUrl, { useCache: false });
-    const coicopEntries = data ? data : [];
+  const searchUrl = `${COICOP_SEARCH_ENDPOINT}?text=${coicopSearchTerm}`
+  const [{ data, loading, error }, refetch] = useAxios(searchUrl, {
+    useCache: false,
+  })
+  const coicopEntries = data ? data : []
 
-    console.log('fetched coicop entries', coicopEntries);
+  console.log('fetched coicop entries', coicopEntries)
 
-    const coicopUpdate = (v) => setCoicopSearchTerm(v);
-    console.log('coicop term', coicopSearchTerm);
+  const coicopUpdate = (v) => setCoicopSearchTerm(v)
+  console.log('coicop term', coicopSearchTerm)
 
-    return {
-        coicopSearchTerm,
-        setCoicopSearchTerm: coicopUpdate,
-        coicopEntries,
-        loading,
-        error,
-    };
-};
+  return {
+    coicopSearchTerm,
+    setCoicopSearchTerm: coicopUpdate,
+    coicopEntries,
+    loading,
+    error,
+  }
+}
