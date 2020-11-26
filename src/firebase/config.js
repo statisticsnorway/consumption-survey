6beb622ed9c01ff587ab461c0249c9afc0bc1cc5
prabu-ssb <prabakar.venkataraman@ssb.no>
2020-11-26 23:17:08 +0100

    cleanup

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyA2XZh8VnD72HK6s0DyP1kpLmyKBC0wvEQ",
-    authDomain: "csfb-6b1f4.firebaseapp.com",
-    databaseURL: "https://csfb-6b1f4.firebaseio.com",
-    projectId: "csfb-6b1f4",
-    storageBucket: "csfb-6b1f4.appspot.com",
-    messagingSenderId: "1091365255450",
-    appId: "1:1091365255450:web:7f64503d2ec330a01154a3"
-};
