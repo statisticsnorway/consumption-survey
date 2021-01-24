937bf910cfb0736c85acbd27019e0d34c4c1f8c8
prabu-ssb <prabakar.venkataraman@ssb.no>
2021-01-24 22:28:22 +0100

    fix header style

--- src/components/common/help/Instructions.tsx
+++ src/components/common/help/Instructions.tsx
@@ -16,7 +16,7 @@ const Instructions = ({steps = []}: InstructionsProps) => {
         <div className={styles.instructions}>
             {steps.map((step, idx) => (
                 <div className={styles.instructionStep}>
-                    <div className={styles.stepNr}>{idx}</div>
+                    <div className={styles.stepNr}>{idx + 1}</div>
                     <div className={styles.text}>{step.text}</div>
                     <div className={styles.figure}>{step.figure}</div>
                 </div>
