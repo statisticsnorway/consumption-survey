62223c765f7a715ca9a23c1a9d94a67c331b13a9
prabu-ssb <prabakar.venkataraman@ssb.no>
2021-04-14 17:42:54 +0200

    FBU-549: auth url and token endpoints

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -3,6 +3,9 @@ import { useContext, useEffect } from 'react';
 import { UserContext } from '../../contexts';
 import Loader from '../common/Loader';
 import PouchDBProvider from '../../pouchdb/PouchDBProvider';
+import getConfig from 'next/config';
+
+const appConfig = getConfig();
 
 const EXCLUDE_AUTH = [
     '/',
@@ -12,9 +15,14 @@ const EXCLUDE_AUTH = [
     '/support/onboarding'
 ];
 
-export default (props) => {
+const ProtectedRoute = (props) => {
     const router = useRouter();
     const {isAuthenticated, isLoggingIn} = useContext(UserContext);
+
+    const getAuthUrl = () => {
+        const {envVars} = appConfig.publicRuntimeConfig;
+        return envVars.NEXT_PUBLIC_AUTH_URL;
+    };
 
     useEffect(() => {
         console.log('isAuthenticated', isAuthenticated);
@@ -22,7 +30,8 @@ export default (props) => {
         console.log('check2', !EXCLUDE_AUTH.includes(router.pathname));
         console.log('final', (!isAuthenticated && (!EXCLUDE_AUTH.includes(router.pathname))));
         if (!isAuthenticated && (!EXCLUDE_AUTH.includes(router.pathname))) {
-            router.push('/auth');
+            console.log('Auth url', getAuthUrl());
+            router.push(getAuthUrl());
         } else {
             /*
             if (router.pathname === '/') {
@@ -48,3 +57,10 @@ export default (props) => {
         return <Loader/>;
     }
 };
+
+ProtectedRoute.getInitialProps = () => {
+    return {};
+};
+
+export default ProtectedRoute;
+
