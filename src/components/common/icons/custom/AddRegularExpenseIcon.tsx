885132431a237ced00706ef5e3569b3f7bd0a6de
prabu-ssb <prabakar.venkataraman@ssb.no>
2021-05-18 11:58:39 +0200

    test n/w v1.0

--- src/components/common/icons/custom/AddRegularExpenseIcon.tsx
+++ src/components/common/icons/custom/AddRegularExpenseIcon.tsx
@@ -5,24 +5,24 @@ export type AddRegularExpenseProps = IconProps & {};
 const AddRegularExpenseIcon = ({width = 64, height = 80}: AddRegularExpenseProps) => (
     <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0)">
-            <path d="M2.8999 30.5001L3.8999 24.6001L27.1999 14.6001L49.6999 24.6001L51.4999 30.5001H2.8999Z" fill="#C3DCDC"/>
-            <path d="M2.8999 30.5H51.4999L27.1999 40.7L2.8999 30.5Z" fill="#274247"/>
-            <path d="M45.5001 45.8L48.8001 28.8L41.1001 15.9L46.1001 1H6.1001L1.1001 15.9L8.8001 28.8L5.7001 45.8H45.5001Z" fill="white" stroke="#274247" stroke-width="2" stroke-miterlimit="10"/>
-            <path d="M51.4999 30.5V60H2.8999V30.5L27.1999 40.7L51.4999 30.5Z" fill="#C3DCDC"/>
-            <path d="M43.4001 78.3002C53.5069 78.3002 61.7001 70.107 61.7001 60.0002C61.7001 49.8934 53.5069 41.7002 43.4001 41.7002C33.2933 41.7002 25.1001 49.8934 25.1001 60.0002C25.1001 70.107 33.2933 78.3002 43.4001 78.3002Z" fill="white" stroke="#274247" stroke-width="2" stroke-miterlimit="10"/>
-            <path d="M10.1001 8.2998H26.6001" stroke="#C3DCDC" stroke-width="2" stroke-miterlimit="10"/>
-            <path d="M14.8 29.1001H43.1" stroke="#C3DCDC" stroke-width="2" stroke-miterlimit="10"/>
-            <path d="M14.3 33.2002H37.3" stroke="#C3DCDC" stroke-width="2" stroke-miterlimit="10"/>
-            <path d="M7.69995 13.1001H17.7" stroke="#C3DCDC" stroke-width="2" stroke-miterlimit="10"/>
-            <path d="M50.7499 52.2144H36.2499C35.1059 52.2144 34.1785 53.0164 34.1785 54.0059V66.9943C34.1785 67.9837 35.1059 68.7858 36.2499 68.7858H50.7499C51.8939 68.7858 52.8213 67.9837 52.8213 66.9943V54.0059C52.8213 53.0164 51.8939 52.2144 50.7499 52.2144Z" stroke="#274247" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M47.6428 50.1431V54.2859" stroke="#274247" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M39.3572 50.1431V54.2859" stroke="#274247" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M35.2144 57.3931H51.7858" stroke="#274247" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <circle cx="39" cy="62" r="2" fill="#274247"/>
+            <path d="M0.100098 30L1.1001 24L24.1001 15L47.1001 24L49.1001 30H0.100098Z" fill="#C3DCDC"/>
+            <path d="M0.100098 30H49.1001L24.1001 40L0.100098 30Z" fill="#274247"/>
+            <path d="M42.1001 46L46.1001 29L39.1001 16L44.1001 1H6.1001L1.1001 16L8.1001 29L2.1001 46H42.1001Z" fill="white" stroke="#274247" stroke-width="2" stroke-miterlimit="10"/>
+            <path d="M49.1001 30V59H0.100098V30L24.1001 40L49.1001 30Z" fill="#C3DCDC"/>
+            <path d="M41.1001 79C51.0412 79 59.1001 70.9411 59.1001 61C59.1001 51.0589 51.0412 43 41.1001 43C31.159 43 23.1001 51.0589 23.1001 61C23.1001 70.9411 31.159 79 41.1001 79Z" fill="white" stroke="#274247" stroke-width="2" stroke-miterlimit="10"/>
+            <path d="M8.1001 8H24.1001" stroke="#C3DCDC" stroke-width="2" stroke-miterlimit="10"/>
+            <path d="M12.1001 28H41.1001" stroke="#C3DCDC" stroke-width="2" stroke-miterlimit="10"/>
+            <path d="M11.1001 32H36.1001" stroke="#C3DCDC" stroke-width="2" stroke-miterlimit="10"/>
+            <path d="M6.1001 13H16.1001" stroke="#C3DCDC" stroke-width="2" stroke-miterlimit="10"/>
+            <path d="M51.1001 52H31.1001V70H51.1001V52Z" fill="white" stroke="#274247" stroke-width="2" stroke-miterlimit="10"/>
+            <path d="M31 57H51" stroke="#274247" stroke-width="2"/>
+            <path d="M36.1001 55V48" stroke="#274247" stroke-width="2" stroke-miterlimit="10"/>
+            <path d="M46.1001 55V48" stroke="#274247" stroke-width="2" stroke-miterlimit="10"/>
+            <path d="M37.5 67C39.433 67 41 65.433 41 63.5C41 61.567 39.433 60 37.5 60C35.567 60 34 61.567 34 63.5C34 65.433 35.567 67 37.5 67Z" fill="#274247"/>
         </g>
         <defs>
             <clipPath id="clip0">
-                <rect width="62.7" height="79.3" fill="white"/>
+                <rect width="60.1" height="80" fill="white"/>
             </clipPath>
         </defs>
     </svg>
