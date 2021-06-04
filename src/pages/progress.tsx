cc216aab49eb44964c235f5ad29480269869857f
Eiliv <evd@ssb.no>
2021-06-04 17:14:10 +0200

    flesh out progress page

--- src/pages/progress.tsx
+++ src/pages/progress.tsx
@@ -1,16 +1,20 @@
 import Workspace from '../components/layout/workspace/Workspace';
 import PageTitle from '../components/common/PageTitle';
-import {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {FireContext, UserContext} from '../contexts';
 import { useTranslation } from 'react-i18next';
 import Accordion from "../components/common/accordion/Accordion";
 import {CheckCircle, Circle} from "react-feather";
+import usePurchases from "../hocs/usePurchases";
+import style from './styles/progress.module.scss'
+import {Divider} from "@material-ui/core";
 
 const Progress = () => {
     const {userInfo, isAuthenticated} = useContext(UserContext);
     const [questionnaireStatus, setQuestionnaireStatus] = useState("NOT_STARTED")
     const { firestore} = useContext(FireContext)
     const {t} = useTranslation('progress');
+    const {purchases} = usePurchases()
     useEffect(() => {
         firestore
             .collection(`/users/${userInfo.userName}/questionnaire`)
@@ -34,53 +38,100 @@ const Progress = () => {
             <Accordion title={t('registerPeriod.showRegisterPeriod')}>
                 Kalender
             </Accordion>
-            <div>
-                <div>
-                    <div style={{display: 'flex'}}>
-                        <CheckCircle/>
-                        <h3>{t('recruitment.title')}</h3>
-                        <div>{t('recruitment.complete.boxText')}</div>
+            <div className={style.progress}>
+                <div className={style.subSection}>
+                    <div className={style.header}>
+                        <CheckCircle size={22}
+                                     color={"green"}/>
+                        <div className={style.headerTitle}>
+                            <h3 style={{margin: 0}} className={'ssb-title'}>{t('recruitment.title')}</h3>
+                            <div className={style.complete}>{t('recruitment.complete.boxText')}</div>
+                        </div>
                     </div>
                     <p>{t('recruitment.complete.innerText')}</p>
                 </div>
-                <div>
+                <hr/>
+                {purchases && purchases.length > 0 &&
+                    <div className={style.subSection}>
+                        <div className={style.header}>
+                            <Circle/>
+                            <div className={style.headerTitle}>
+                                <h3 style={{margin: 0}} className={'ssb-title'}>{t('registerExpenses.title')}</h3>
+                                <div className={style.started}>{t('registerExpenses.ongoing.boxText')}</div>
+                            </div>
+                        </div>
+                        <p>{t('registerExpenses.notStarted.innerTextPre')}{userInfo.respondentDetails.diaryStart}{' - '}{userInfo.respondentDetails.diaryEnd}{t('registerExpenses.notStarted.innerTextPost')}</p>
+                    </div>
 
+                }
+                {(!purchases || purchases.length < 1) &&
+                <div className={style.subSection}>
+                    <div className={style.header}>
+                        <Circle/>
+                        <div className={style.headerTitle}>
+                            <h3 style={{margin: 0}} className={'ssb-title'}>{t('registerExpenses.title')}</h3>
+                            <div className={style.notStarted}>{t('registerExpenses.notStarted.boxText')}</div>
+                        </div>
+                    </div>
+                    <p>{t('registerExpenses.notStarted.innerTextPre')}{userInfo.respondentDetails.diaryStart}{' - '}{userInfo.respondentDetails.diaryEnd}{t('registerExpenses.notStarted.innerTextPost')}</p>
                 </div>
 
+                }
+                <hr/>
                 {questionnaireStatus === 'COMPLETE' &&
-                    <div>
-                        <div>
-                            <CheckCircle/>
-                            <h3>{t('questionnaire.title')}</h3>
-                            <div>{t('questionnaire.complete.boxText')}</div>
+                    <div className={style.subSection}>
+                        <div className={style.header}>
+                            <CheckCircle size={22}
+                                         color={"green"}/>
+                            <div className={style.headerTitle}>
+                                <h3 style={{margin: 0}} className={'ssb-title'}>{t('questionnaire.title')}</h3>
+                                <div className={style.complete}>{t('questionnaire.complete.boxText')}</div>
+                            </div>
                         </div>
                         <p>{t('questionnaire.complete.innerText')}</p>
                     </div>
 
                 }
                 {questionnaireStatus === 'STARTED' &&
-                <div>
-                    <div>
+                <div className={style.subSection}>
+                    <div className={style.header}>
                         <Circle/>
-                        <h3>{t('questionnaire.title')}</h3>
-                        <div>{t('questionnaire.onGoing.boxText')}</div>
+                        <div className={style.headerTitle}>
+                            <h3 style={{margin: 0}} className={'ssb-title'}>{t('questionnaire.title')}</h3>
+                            <div className={style.started}>{t('questionnaire.onGoing.boxText')}</div>
+                        </div>
                     </div>
                     <p>{t('questionnaire.onGoing.innerText')}</p>
                 </div>
 
                 }
                 {questionnaireStatus === 'NOT_STARTED' &&
-                <div>
-                    <div>
+                <div className={style.subSection}>
+                    <div style={{display: 'flex'}}>
                         <Circle/>
-                        <h3>{t('questionnaire.title')}</h3>
-                        <div>{t('questionnaire.notStarted.boxText')}</div>
+                        <div className={style.headerTitle}>
+                            <h3 style={{margin: 0}} className={'ssb-title'}>{t('questionnaire.title')}</h3>
+                            <div className={style.notStarted}>{t('questionnaire.notStarted.boxText')}</div>
+                        </div>
                     </div>
                     <p>{t('questionnaire.notStarted.innerText')}</p>
                 </div>
 
                 }
-
+                <hr/>
+                <div className={style.completeSurvey}>
+                    <h3>{t('completeSurvey.title')}</h3>
+                    {questionnaireStatus === 'COMPLETE' &&
+                        <p>{t('completeSurvey.finishedText')}</p>
+                    }
+                    {questionnaireStatus !== 'COMPLETE' &&
+                    <p>{t('completeSurvey.notFinishedText')}</p>
+                    }
+                    <hr/>
+                    <button style={{width: '100%'}} className={`ssb-btn primary-btn`} disabled={questionnaireStatus !== 'COMPLETE'}>
+                        {t('completeSurvey.completeButtonText')}
+                    </button>
+                </div>
             </div>
         </Workspace>
     );
