4b0e9a33867d20fef449622c063e82f6dbc96f13
Eiliv <evd@ssb.no>
2021-06-04 14:24:16 +0200

    flesh out progress page

--- src/pages/progress.tsx
+++ src/pages/progress.tsx
@@ -1,16 +1,64 @@
 import Workspace from '../components/layout/workspace/Workspace';
 import PageTitle from '../components/common/PageTitle';
-import { useContext } from 'react';
-import { UserContext } from '../contexts';
+import {useContext, useEffect, useState} from 'react';
+import {FireContext, UserContext} from '../contexts';
 import { useTranslation } from 'react-i18next';
+import Accordion from "../components/common/accordion/Accordion";
+import {CheckCircle, Circle} from "react-feather";
 
 const Progress = () => {
     const {userInfo, isAuthenticated} = useContext(UserContext);
+    const [questionnaireStatus, setQuestionnaireStatus] = useState("NOT_STARTED")
+    const { firestore} = useContext(FireContext)
     const {t} = useTranslation('progress');
+    useEffect(() => {
+        firestore
+            .collection(`/users/${userInfo.userName}/questionnaire`)
+            .doc('data')
+            .get()
+            .then((doc) => {
+                if (doc.exists) {
+                    const data = doc.data()
+                    setQuestionnaireStatus(data.status)
+                }
+            })
+            .catch((err) => {
+                console.log('cannot update answers', err)
+            })
+    }, [])
     console.log('userInfo', isAuthenticated, userInfo);
     return (
         <Workspace>
             <PageTitle title={t('title')} />
+            <p>{t('intro')}</p>
+            <Accordion title={t('registerPeriod.showRegisterPeriod')}>
+                Kalender
+            </Accordion>
+            <div>
+                <div>
+                    <div style={{display: 'flex'}}>
+                        <CheckCircle/>
+                        <h3>{t('recruitment.title')}</h3>
+                        <div>{t('recruitment.complete.boxText')}</div>
+                    </div>
+                    <p>{t('recruitment.complete.innerText')}</p>
+                </div>
+                <div>
+
+                </div>
+
+                {questionnaireStatus === 'COMPLETE' &&
+                    <div>
+                        <div>
+                            <CheckCircle/>
+                            <h3>{t('questionnaire.title')}</h3>
+                            <div>{t('questionnaire.complete.boxText')}</div>
+                        </div>
+                    </div>
+
+                }
+
+            </div>
         </Workspace>
     );
 };
